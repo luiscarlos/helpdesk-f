830b9cf461c3b203c1e489c505636ddcfb5314ce
luiscarlos <luiscarlos8pe@gmail.com>
2023-09-17 14:03:14 -0300

    Componente LOGIN criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 // import { HeaderComponent } from './components/header/header.component';
 // import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
@@ -59,7 +60,8 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
     NavComponent,
     HomeComponent,
     HeaderComponent,
-    TecnicoListComponent
+    TecnicoListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
