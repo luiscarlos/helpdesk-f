2b4255fcd0079c6b0fb10cf27ae57314c003cf3a
luiscarlos <luiscarlos8pe@gmail.com>
2023-09-21 19:46:30 -0300

    Interceptor para requisiçoes http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { LoginComponent } from './components/login/login.component';
 // import { HeaderComponent } from './components/header/header.component';
 // import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 //import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 // import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 //import { NgxMaskModule } from 'ngx-mask';
@@ -93,7 +94,7 @@ import { ToastrModule } from 'ngx-toastr';
       progressBar: true
     }),
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
