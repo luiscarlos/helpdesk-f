a7d15e420f7d516df9f89e99cd6b4bd58b975a0e
luiscarlos <luiscarlos8pe@gmail.com>
2023-10-02 20:50:33 -0300

    Rota para componente TECNICO-CREATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, createComponent } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 
 const routes: Routes = [
@@ -12,9 +13,9 @@ const routes: Routes = [
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
-      { path: 'tecnicos',  component:   TecnicoListComponent },
 
-     
+      { path: 'tecnicos',  component:   TecnicoListComponent },
+      { path: 'tecnicos/create',  component:   TecnicoCreateComponent }
     ]
   }
 ];
