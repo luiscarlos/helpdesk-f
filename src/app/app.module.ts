ad4ce5602d05cd9bc1bb531de765b316e882486a
luiscarlos <luiscarlos8pe@gmail.com>
2023-10-02 20:25:26 -0300

    componente TECNICO-CREATE criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { LoginComponent } from './components/login/login.component';
 // import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 //import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 // import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 //import { NgxMaskModule } from 'ngx-mask';
@@ -61,7 +62,8 @@ import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
     HomeComponent,
     HeaderComponent,
     TecnicoListComponent,
-    LoginComponent
+    LoginComponent,
+    TecnicoCreateComponent
   ],
   imports: [
     BrowserModule,
