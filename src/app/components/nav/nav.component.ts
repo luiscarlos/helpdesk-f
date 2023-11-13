a1d6874f4f784c9f5cf7a3b74805f80c1c36a4b0
luiscarlos <luiscarlos8pe@gmail.com>
2023-11-13 17:56:29 -0300

    Ajuste ao atualizar senha

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -27,6 +27,6 @@ export class NavComponent implements OnInit {
   logout() {
     this.router.navigate(['login'])
     this.authService.logout();
-    this.toast.info('Logout realizado com sucesso', 'Logout',{timeOut: 70000})
+    this.toast.info('Logout realizado com sucesso', 'Logout',{timeOut: 30000})
   }
 }
