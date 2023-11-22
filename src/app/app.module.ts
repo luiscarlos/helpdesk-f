9588610fb54ef228418562cd555d1eb570311dbb
luiscarlos <luiscarlos8pe@gmail.com>
2023-11-22 20:28:28 -0300

    Componente CHAMADO-LIST criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,6 +50,7 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 // import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 // import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 // import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
@@ -69,7 +70,8 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
     ClienteCreateComponent,
     ClienteDeleteComponent,
     ClienteListComponent,
-    ClienteUpdateComponent
+    ClienteUpdateComponent,
+    ChamadoListComponent
   ],
   imports: [
     BrowserModule,
